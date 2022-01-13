a3cca19f3f93b2969fb33911476a4b2f8e540123
zl <zl@ty-zl>
2022-01-13 17:29:09 +0800

    Initial commit

--- stu/stu_QLineEdit4.py
+++ stu/stu_QLineEdit4.py
@@ -11,28 +11,29 @@ class Winform(QWidget):
         self.initUI()
 
         flo = QFormLayout()     # 添加表单布局
-        pIPLineEdit = QLineEdit()
-        pMACLineEdit = QLineEdit()
-        pDATELineEdit = QLineEdit()
-        plicenseLineEdit = QLineEdit()
+        e1 = QLineEdit()
+        e2 = QLineEdit()
+        e3 = QLineEdit()
+        e4 = QLineEdit()
+        e5 = QLineEdit()
+        e6 = QLineEdit()
 
-        # 设置掩码格式
-        pIPLineEdit.setInputMask('000.000.000.000;_')
-        pMACLineEdit.setInputMask('HH:HH:HH:HH:HH:HH:HH:HH;_')
-        pDATELineEdit.setInputMask('0000-00-00;_')
-        plicenseLineEdit.setInputMask('>AAAAA-AAAAA-AAAAA;#')
+        e1.setValidator(QIntValidator())        # 设置成只允许输入数字
+        e3.setInputMask('000.000.000.000;_')     # 设置掩码格式
 
         # 将QLineEdit添加到表单布局
-        flo.addRow('IP', pIPLineEdit)
-        flo.addRow('MAC', pMACLineEdit)
-        flo.addRow('日期', pDATELineEdit)
-        flo.addRow('许可号', plicenseLineEdit)
+        flo.addRow('integer validator整数', e1)
+        flo.addRow('Double validator浮点数', e2)
+        flo.addRow('Input Mask子网掩码', e3)
+        flo.addRow('Text changed文本', e4)
+        flo.addRow('Password密码', e5)
+        flo.addRow('Read only只读', e6)
 
         self.setLayout(flo)
     # 初始化窗口
     def initUI(self):
         self.setGeometry(200,300,400,150)
-        self.setWindowTitle('掩码输入实例')
+        self.setWindowTitle('QLineEdit综合实例')
         self.setWindowIcon(QIcon('img/74.ico'))
 
 
